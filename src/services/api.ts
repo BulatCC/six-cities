43c0bcfa84ef46f45c2a926644e1acced6360854
Булат <50518242+BulatCC@users.noreply.github.com>
2023-06-01 22:24:28 +0500

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosInstance, AxiosRequestConfig } from 'axios';
 import { getToken } from './token';
 
-const BACKEND_URL = 'https://9.react.pages.academy/six-cities';
+const BACKEND_URL = 'https://12.react.pages.academy/six-cities';
 const REQUEST_TIMEOUT = 4000;
 
 export const createApi = (): AxiosInstance => {
